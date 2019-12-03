866509affc8c117a330035942d2766982ec2a319
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2019-12-02 22:23:15 -0500

    Update Bishop.java

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -14,18 +14,20 @@ public class Bishop extends Piece{
     protected boolean isValid(Point start, Point end){
         if(super.isValid(start, end)){
             if(Math.abs(end.getY() - start.getY()) == Math.abs(end.getX() - start.getX())){
-                for(int i = 0; i < Math.abs(start.getY() - end.getY()); i++){
-                    System.out.println(start.getX() + i + " " + (start.getY() + i));
+                for(int i = 0; i < Math.abs(start.getY() - end.getY()) - 1; i++){
+                    System.out.println(start.getX() - i + " " + (start.getY() + i));
+                    System.out.println(Main.getBoard()[start.getX() - i][start.getY() + i]);
                     if(start.getX() < end.getX() && start.getY() < end.getY())
                         if(start.getX() + i < Main.getBoard().length && start.getY() + i < start.getX() && !Main.getBoard()[start.getX() + i][start.getY() + i].isPlaceHolder()){
                             System.out.println(1);
                             return false;
                         }
                     if(start.getX() > end.getX() && start.getY() < end.getY())
-                        if(start.getX() - i >= 0 && start.getY() + i < Main.getBoard().length && !Main.getBoard()[start.getX() - i][start.getY() + i].isPlaceHolder()){
-                            System.out.println(2);
-                            return false;
-                        }
+                        if(start.getX() - i >= 0 && start.getY() + i < Main.getBoard().length)
+                            if(!Main.getBoard()[start.getX() - i][start.getY() + i].isPlaceHolder()){
+                                System.out.println(2);
+                                return false;
+                            }
                     if(start.getX() < end.getX() && start.getY() > end.getY())
                         if(start.getX() + i < Main.getBoard().length && start.getY() - i >= 0 && !Main.getBoard()[start.getX() + i][start.getY() - i].isPlaceHolder()){
                             System.out.println(3);
