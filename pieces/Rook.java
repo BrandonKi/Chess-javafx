60982e967c93d5cbb29dbb6a22a17101534b317d
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2019-12-04 15:13:31 -0500

    Update Rook.java

--- pieces/Rook.java
+++ pieces/Rook.java
@@ -12,9 +12,44 @@ public class Rook extends Piece {
     }
  
     protected boolean isValid(Point start, Point end){
-        return super.isValid(start, end);
+        if(super.isValid(start, end) && (Math.abs(end.getY() - start.getY()) == 0 || Math.abs(end.getX() - start.getX()) == 0)){
+            short difX = (short)(end.getX() - start.getX());
+            short difY = (short)(end.getY() - start.getY());
+            System.out.println(difX + " " + difY);
+            if(difY > 0 && difX == 0){
+                System.out.println(1);
+                for(short i = 1; i < difY; i++){
+                    System.out.println(Main.getBoard()[end.getX()][end.getY() - i] + " " + (end.getX()) + ", " + (end.getY() - i));
+                    if(!Main.getBoard()[end.getX()][end.getY() - i].isPlaceHolder())
+                        return false;
+                }
+            }
+            else if(difY < 0 && difX == 0){
+                System.out.println(2);  
+                for(short i = 1; i < difX; i++){
+                    System.out.println(Main.getBoard()[end.getX()][end.getY() + i] + " " + (end.getX()) + ", " + (end.getY() + i));
+                    if(!Main.getBoard()[end.getX()][end.getY() + i].isPlaceHolder())
+                        return false;
+                }
+            }
+            else if(difY == 0 && difX > 0){
+                System.out.println(3);
+                for(short i = 1; i < -difX; i++){
+                    System.out.println(Main.getBoard()[end.getX() - i][end.getY()] + " " + (end.getX() - i) + ", " + (end.getY()));
+                    if(!Main.getBoard()[end.getX() - i][end.getY()].isPlaceHolder())
+                        return false;
+                }
+            }
+            else if(difY == 0 && difX < 0){
+                System.out.println(4);
+                for(short i = 1; i < -difX; i++){
+                    System.out.println(Main.getBoard()[end.getX() + i][end.getY()] + " " + (end.getX() + i) + ", " + (end.getY()));
+                    if(!Main.getBoard()[end.getX() + i][end.getY()].isPlaceHolder())
+                        return false;
+                }
+            }       
+            return true;
+        }
+        return false;
     }
-
-    
-
 }
